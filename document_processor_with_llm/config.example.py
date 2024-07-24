0201d7c97e99d275ef4bf9c8ebb210a687796734
arya2004 <arya20j4@gmail.com>
2024-07-24 22:28:45 +0530

    feat: gitkeep and config example

---
+++ document_processor_with_llm/config.example.py
@@ -0,0 +1,16 @@
+# Configuration file for setting variables
+
+# OpenAI API configuration
+OPENAI_API_KEY = 'your_openai_api_key'
+OPENAI_API_BASE = 'your_openai_api_base'
+
+
+DATA_PATH = "../data/"
+CHUNK_SIZE = 500
+MODEL_NAME = "BAAI/bge-large-en"
+TOP_K = 5
+EXCEL_FILE_PATH = '../output/extracted_points_2.xlsx'
+DOC_FILE_PATH = '../data/bid.docx'
+LOG_FILE_PREFIX = "debug_log_"
+LLM_MODEL = "mistral:latest"
+LOGS_DIR = 'logs/'
